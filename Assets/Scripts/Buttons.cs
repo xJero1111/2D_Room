0cab315e2f234551284078e3f5cd8489c94d880d
xJero1111 <jeronimo.rios.1111@gmail.com>
2026-04-22 06:56:55 -0500

    creé botón

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Buttons : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame ()
     {
-        
+        SceneManager.LoadScene(1);
     }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
